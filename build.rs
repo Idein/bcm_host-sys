14662bb2609091087f1876b0acc855e2b9448d8a
takayuki <takayuki@idein.jp>
2018-01-05 11:08:34 +0900

    rustfmt

--- build.rs
+++ build.rs
@@ -5,27 +5,37 @@ use std::path::{Path, PathBuf};
 
 fn main() {
     // Tell cargo to tell rustc to link the system bcm_host shared library.
-	println!("cargo:rustc-link-lib=bcm_host");
-	println!("cargo:rustc-link-lib=vchiq_arm");
-	println!("cargo:rustc-link-lib=vcos");
-	println!("cargo:rustc-link-lib=pthread");
+    println!("cargo:rustc-link-lib=bcm_host");
+    println!("cargo:rustc-link-lib=vchiq_arm");
+    println!("cargo:rustc-link-lib=vcos");
+    println!("cargo:rustc-link-lib=pthread");
 
-	let mut include_dirs = Vec::new();
+    let mut include_dirs = Vec::new();
 
-	// Add Paths to directories of VideoCore firmware header
-	let dir = env::var("VC_INCLUDE_DIR").unwrap_or("/opt/vc/include".into());
-	include_dirs.push(Path::new(&dir).into());
-	include_dirs.push(Path::new(&dir).join("interface").join("vmcs_host").join("linux"));
-	include_dirs.push(Path::new(&dir).join("interface").join("vcos").join("pthreads"));
+    // Add Paths to directories of VideoCore firmware header
+    let dir = env::var("VC_INCLUDE_DIR").unwrap_or("/opt/vc/include".into());
+    include_dirs.push(Path::new(&dir).into());
+    include_dirs.push(Path::new(&dir).join("interface").join("vmcs_host").join(
+        "linux",
+    ));
+    include_dirs.push(Path::new(&dir).join("interface").join("vcos").join(
+        "pthreads",
+    ));
 
-	// Add Path to directories of Clang header
-	include_dirs.push(Path::new(&env::var("CLANG_INCLUDE_DIR")
-								.expect("CLANG_INCLUDE_DIR like: /usr/lib/llvm-3.9/lib/clang/3.9.1/include")).into());
+    // Add Path to directories of Clang header
+    include_dirs.push(
+        Path::new(&env::var("CLANG_INCLUDE_DIR").expect(
+            "CLANG_INCLUDE_DIR like: /usr/lib/llvm-3.9/lib/clang/3.9.1/include",
+        )).into(),
+    );
 
-	let args: Vec<&str> = include_dirs.iter().flat_map(|path| vec!["-I", path.to_str().unwrap()]).collect();
-	// for &s in &args {
-	// 	debug!("> {}", s);
-	// }
+    let args: Vec<&str> = include_dirs
+        .iter()
+        .flat_map(|path| vec!["-I", path.to_str().unwrap()])
+        .collect();
+    // for &s in &args {
+    // 	debug!("> {}", s);
+    // }
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -36,10 +46,6 @@ fn main() {
         .header("wrapper.h")
 		.clang_args(&["-D", "USE_VCHIQ_ARM"])
 		.clang_args(&args)
-		//.clang_args(&["-I", "/usr/lib/llvm-3.9/lib/clang/3.9.1/include"])
-		//.clang_args(&["-I", "/opt/vc/include"])
-		//.clang_args(&["-I", "/opt/vc/include/interface/vmcs_host/linux"])
-		//.clang_args(&["-I", "/opt/vc/include/interface/vcos/pthreads"])
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
@@ -51,4 +57,3 @@ fn main() {
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 }
-
