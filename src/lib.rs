007a39846b54df0e010f2289b7e6d3c78f8baee1
takayuki <takayuki@idein.jp>
2018-03-14 22:31:38 -0700

    unify lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,17 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-mod vcos_platform;
-use vcos_platform::*;
+use std::mem;
+use std::ptr;
+
+/// sleep a while
+///
+/// /opt/vc/include/vcos_platform.h
+pub fn vcos_sleep(ms: u32) {
+    unsafe {
+        let mut ts: timespec = mem::uninitialized();
+        ts.tv_sec = (ms / 1000) as i32;
+        ts.tv_nsec = (ms % 1000 * 1000000) as i32;
+        nanosleep(&ts, ptr::null_mut());
+    }
+}
